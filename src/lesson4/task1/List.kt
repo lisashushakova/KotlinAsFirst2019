2e8d16bbaadb28b867473283dad4c7ddc89ea389
lisashushakova <lisashushakova@gmail.com>
2019-10-13 23:33:47 +0300

    Шушакова Елизавета Александровна, группа 3530901/90001. lessons 1, 2, 3.

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -282,7 +282,46 @@ fun convert(n: Int, base: Int): List<Int> {
  * Использовать функции стандартной библиотеки, напрямую и полностью решающие данную задачу
  * (например, n.toString(base) и подобные), запрещается.
  */
-fun convertToString(n: Int, base: Int): String = TODO()
+fun convertToString(n: Int, base: Int): String {
+    fun lat (n: Int): String {
+        return when (n) {
+            10 -> "a"
+            11 -> "b"
+            12 -> "c"
+            13 -> "d"
+            14 -> "e"
+            15 -> "f"
+            16 -> "g"
+            17 -> "h"
+            18 -> "i"
+            19 -> "j"
+            20 -> "k"
+            21 -> "l"
+            22 -> "m"
+            23 -> "n"
+            24 -> "o"
+            25 -> "p"
+            26 -> "q"
+            27 -> "r"
+            28 -> "s"
+            29 -> "t"
+            30 -> "u"
+            31 -> "v"
+            32 -> "w"
+            33 -> "x"
+            34 -> "y"
+            35 -> "z"
+            else -> "$n"
+        }
+    }
+    var x = n
+    var result = String()
+    while (x > 0) {
+        result += lat(x % base)
+        x /= base
+    }
+    return result.reversed()
+}
 
 /**
  * Средняя
@@ -311,7 +350,44 @@ fun decimal(digits: List<Int>, base: Int): Int {
  * Использовать функции стандартной библиотеки, напрямую и полностью решающие данную задачу
  * (например, str.toInt(base)), запрещается.
  */
-fun decimalFromString(str: String, base: Int): Int = TODO()
+fun decimalFromString(str: String, base: Int): Int {
+    fun num(str: String): Int {
+        return when (str) {
+            "a" -> 10
+            "b" -> 11
+            "c" -> 12
+            "d" -> 13
+            "e" -> 14
+            "f" -> 15
+            "g" -> 16
+            "h" -> 17
+            "i" -> 18
+            "j" -> 19
+            "k" -> 20
+            "l" -> 21
+            "m" -> 22
+            "n" -> 23
+            "o" -> 24
+            "p" -> 25
+            "q" -> 26
+            "r" -> 27
+            "s" -> 28
+            "t" -> 29
+            "u" -> 30
+            "v" -> 31
+            "w" -> 32
+            "x" -> 33
+            "y" -> 34
+            "z" -> 35
+            else -> str.toInt()
+        }
+    }
+    var result = 0
+    for ((index, element) in str.reversed().withIndex()) {
+        result += num(element.toString()) * (base.toDouble().pow(index)).toInt()
+    }
+    return result
+}
 
 /**
  * Сложная
