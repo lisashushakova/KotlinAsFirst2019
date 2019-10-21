acd08519236959999e4edf465ba7e385ac296472
lisashushakova <lisashushakova@gmail.com>
2019-10-21 23:38:29 +0300

    lesson 4

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -91,7 +91,14 @@ fun buildWordSet(text: List<String>): MutableSet<String> {
  *   buildGrades(mapOf("Марат" to 3, "Семён" to 5, "Михаил" to 5))
  *     -> mapOf(5 to listOf("Семён", "Михаил"), 3 to listOf("Марат"))
  */
-fun buildGrades(grades: Map<String, Int>): Map<Int, List<String>> = TODO()
+fun buildGrades(grades: Map<String, Int>): Map<Int, List<String>> {
+    val result = mutableMapOf<Int, MutableList<String>>()
+    for ((name, grade) in grades) {
+        if (result[grade] == null) result[grade] = mutableListOf(name)
+        else result[grade]?.add(name)
+    }
+    return result
+}
 
 /**
  * Простая
