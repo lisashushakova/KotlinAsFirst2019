dd11119c8e76501ae59c5f1b588bf8ad6d2c3ea2
lisashushakova <lisashushakova@gmail.com>
2019-10-27 15:59:13 +0300

    lesson 4

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -159,7 +159,15 @@ fun whoAreInBoth(a: List<String>, b: List<String>): List<String> = b.intersect(a
  *     mapOf("Emergency" to "911", "Police" to "02")
  *   ) -> mapOf("Emergency" to "112, 911", "Police" to "02")
  */
-fun mergePhoneBooks(mapA: Map<String, String>, mapB: Map<String, String>): Map<String, String> = TODO()
+fun mergePhoneBooks(mapA: Map<String, String>, mapB: Map<String, String>): Map<String, String> {
+    val result = mutableMapOf<String, String>()
+    for ((key, value) in mapA) result += key to value
+    for ((key, value) in mapB) {
+        if (key in result && result[key] != value) result[key] += ", $value"
+        else result += key to value
+    }
+    return result
+}
 
 /**
  * Средняя
@@ -171,7 +179,22 @@ fun mergePhoneBooks(mapA: Map<String, String>, mapB: Map<String, String>): Map<S
  *   averageStockPrice(listOf("MSFT" to 100.0, "MSFT" to 200.0, "NFLX" to 40.0))
  *     -> mapOf("MSFT" to 150.0, "NFLX" to 40.0)
  */
-fun averageStockPrice(stockPrices: List<Pair<String, Double>>): Map<String, Double> = TODO()
+fun averageStockPrice(stockPrices: List<Pair<String, Double>>): Map<String, Double> {
+    val result = mutableMapOf<String, Double>()
+    val average = mutableMapOf<String, Int>()
+    for ((key, value) in stockPrices) {
+        if (key !in result) {
+            result += key to value
+            average += key to 1
+        }
+        if (value != result[key]) {
+            result[key] = result[key]!! + value
+            average[key] = average[key]!! + 1
+        }
+    }
+    for (key in result.keys) result[key] = result[key]!! / average[key]!!
+    return result
+}
 
 /**
  * Средняя
